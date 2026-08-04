6e3b1fedb1a4855793b3ae74b82d7ee278b3cdf8
caruasdo-7689 <solitarior.torre2@gmail.com>
2026-08-04 15:21:59 -0400

    deploy quiz game

--- gamequiz/quiz-survivor.service.worker.js
+++ gamequiz/quiz-survivor.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785864531|8836464';
+const CACHE_VERSION = '1785871175|29105065';
 /** @type {string} */
 const CACHE_PREFIX = 'Quiz Survivor-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
