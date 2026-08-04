ef64fc108826ecd7c65e1c52ff0f36b2e593a0d6
caruasdo-7689 <solitarior.torre2@gmail.com>
2026-08-04 13:43:48 -0400

    game quiz optimizando

--- gamequiz/quiz-survivor.service.worker.js
+++ gamequiz/quiz-survivor.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785859227|261528904';
+const CACHE_VERSION = '1785864531|8836464';
 /** @type {string} */
 const CACHE_PREFIX = 'Quiz Survivor-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
